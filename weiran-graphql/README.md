0c70a96a5fe58d82ee5768eb3785ab487dd05060
weiran1999 <907221539@qq.com>
2023-01-27 19:47:17 +0800

    feat: init graphql

---
+++ weiran-graphql/README.md
@@ -0,0 +1,2 @@
+# 参考
+- [Graphql+SpringBoot看这一篇就够了](https://juejin.cn/post/7066694688090095652)
---
+++ weiran-graphql/src/main/java/com/weiran/graphql/GraphqlApplication.java
@@ -0,0 +1,16 @@
+package com.weiran.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class GraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlApplication.class, args);
+    }
+
+}
