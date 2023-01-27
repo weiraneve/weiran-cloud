09df56ec4d87d318c1767b8accb68b7fc9973b36
weiran1999 <907221539@qq.com>
2023-01-27 17:16:50 +0800

    fix: fix code structure

--- weiran-admin/src/main/java/com/weiran/admin/AdminApplication.java
+++ weiran-admin/src/main/java/com/weiran/admin/AdminApplication.java
@@ -4,9 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- *
- */
+
 @SpringBootApplication
 @EnableAdminServer
 public class AdminApplication {
