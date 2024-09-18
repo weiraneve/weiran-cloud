fabcc0f122e5b5e2fea3973a71d777acf33427a7
weiraneve <907221539@qq.com>
2024-09-18 15:43:06 +0800

    feat: add mongodb repo example code

--- weiran-db/src/main/java/com/weiran/cloud/repository/UserRepository.java
+++ weiran-db/src/main/java/com/weiran/cloud/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.weiran.cloud.repository;
 
 import com.weiran.cloud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
 
+//public interface UserRepository extends MongoRepository<User, String> {
+//    User findByEmail(String email);
+//}
